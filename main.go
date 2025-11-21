2110d9ab6cecbba63f5f523da9cac277de6af7ae
pemerick <emerick.pacaud@ynov.com>
2025-11-21 09:45:57 +0100

    Utilisation de l'API

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Artiste struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+func main() {
+	fmt.Println("début")
+	Api()
+}
+
+func Api() {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	data := string(body)
+	//fmt.Println(string(body))
+
+	var artiste []Artiste
+	err := json.Unmarshal([]byte(data), &artiste)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return
+	}
+	fmt.Println()
+	fmt.Println(artiste[0])
+}
