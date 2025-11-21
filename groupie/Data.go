58563d378512b6cd99046e8e5ca7d7c8768f73b3
pemerick <emerick.pacaud@ynov.com>
2025-11-21 11:18:40 +0100

    amélioration de la récupération API

--- groupie/Data.go
+++ groupie/Data.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-type Artiste struct {
+type ArtisteS struct {
 	Id           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
@@ -19,21 +19,104 @@ type Artiste struct {
 	Relations    string   `json:"relations"`
 }
 
+type LocationsS struct {
+	Id        int    `json:"id"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+}
+
+type DatesS struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type RelationS struct {
+	Id             int               `json:"id"`
+	DatesLocations map[string]string `json:"datesLocations"`
+}
+
 func ChargerLesDonnées() {
-	url := "https://groupietrackers.herokuapp.com/api/artists"
+	bloque := "https://groupietrackers.herokuapp.com/api/"
+	// fmt.Println(Ressource(bloque + "locations"))
+	listeDesArtiste := ChargerLesArtistes(bloque + "artists")
+	listeDesLocations := ChargerLesLocations(bloque + "locations")
+	listeDesDates := ChargerLesDates(bloque + "dates")
+	listeDesRelation := ChargerLesRelation(bloque + "relation")
+
+	fmt.Println(len(listeDesArtiste))
+	fmt.Println(len(listeDesLocations))
+	fmt.Println(len(listeDesDates))
+	fmt.Println(len(listeDesRelation))
+}
+
+func Ressource(url string) string {
 	req, _ := http.NewRequest("GET", url, nil)
 	res, _ := http.DefaultClient.Do(req)
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-	data := string(body)
-	//fmt.Println(string(body))
+	valeur := string(body)
+	if valeur[0] != '[' {
+		total := "" //{"index":
+		if url == "https://groupietrackers.herokuapp.com/api/locations" {
+			for i := 9; i < len(valeur)-2; i++ {
+				total += string(valeur[i])
+			}
 
-	var artiste []Artiste
+		} else if url == "https://groupietrackers.herokuapp.com/api/dates" {
+			for i := 9; i < len(valeur)-2; i++ {
+				total += string(valeur[i])
+			}
+		} else if url == "https://groupietrackers.herokuapp.com/api/relation" {
+			for i := 9; i < len(valeur)-2; i++ {
+				total += string(valeur[i])
+			}
+			// fmt.Println(total)
+		}
+		// fmt.Println(total)
+		valeur = total
+	}
+	fmt.Println()
+	// fmt.Println(string(body))
+	return valeur
+}
+
+func ChargerLesArtistes(url string) []ArtisteS {
+	data := Ressource(url)
+	var artiste []ArtisteS
 	err := json.Unmarshal([]byte(data), &artiste)
 	if err != nil {
 		fmt.Println("Erreur :", err)
-		return
+		return []ArtisteS{}
 	}
-	fmt.Println()
-	fmt.Println(artiste[0])
+	return artiste
+}
+func ChargerLesLocations(url string) []LocationsS {
+	data := Ressource(url)
+	var locations []LocationsS
+	err := json.Unmarshal([]byte(data), &locations)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return []LocationsS{}
+	}
+	return locations
+}
+func ChargerLesDates(url string) []DatesS {
+	data := Ressource(url)
+	var dates []DatesS
+	err := json.Unmarshal([]byte(data), &dates)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return []DatesS{}
+	}
+	return dates
+}
+func ChargerLesRelation(url string) []RelationS {
+	data := Ressource(url)
+	var relation []RelationS
+	err := json.Unmarshal([]byte(data), &relation)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return []RelationS{}
+	}
+	return relation
 }
--- main.go
+++ main.go
@@ -5,18 +5,6 @@ import (
 	"groupie"
 )
 
-type Artiste struct {
-	Id           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
-
 func main() {
 	fmt.Println("début")
 	groupie.ChargerLesDonnées()
