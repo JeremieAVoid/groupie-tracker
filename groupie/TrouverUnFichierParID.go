7297127b6388a9d1eaf7059d9ba1aca1e5380292
pemerick <emerick.pacaud@ynov.com>
2025-12-19 09:15:45 +0100

    Amélioration du CSS, ajouts des membre dans la page information et dans la page template

--- groupie/TrouverUnFichierParID.go
+++ groupie/TrouverUnFichierParID.go
@@ -4,7 +4,6 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 // Cette fonction doit être appeller uniquement si la liste est d'une taille supérieur à 0
@@ -67,7 +66,6 @@ type PageData struct {
 	Prénom                string
 	Image                 string
 	DateDeCréation        string
-	Membres               string
 	PremierAlbum          string
 	VisiblePrénom         string
 	VisibleImage          string
@@ -75,58 +73,7 @@ type PageData struct {
 	VisibleMembres        string
 	VisiblePremierAlbum   string
 	Id                    string
-}
-
-func PlacerLesRésultaDeRecherche(w http.ResponseWriter, r *http.Request, listeID []int, lotDeListe LotDeListe) {
-	//bloc principale :
-	data := PageData{
-		Prénom:                "",
-		Image:                 "",
-		DateDeCréation:        "",
-		Membres:               "",
-		PremierAlbum:          "",
-		VisiblePrénom:         "",
-		VisibleImage:          "invisible",
-		VisibleDateDeCréation: "",
-		VisibleMembres:        "",
-		VisiblePremierAlbum:   "",
-	}
-	PlacerUnePage(w, r, data, "HTML/main.html")
-
-	//page 1 :
-	for i := 0; i < len(listeID); i++ {
-		blocArtiste := TrouverUnElementParID_ArtisteS(listeID[i], lotDeListe.listeDesArtistes)
-		data2 := PageData{
-			Prénom:         blocArtiste.Name,
-			Image:          blocArtiste.Image,
-			DateDeCréation: strconv.Itoa(blocArtiste.CreationDate),
-			Membres:        blocArtiste.Members[0],
-			PremierAlbum:   blocArtiste.FirstAlbum,
-			Id:             strconv.Itoa(i + 1),
-		}
-		if r.FormValue("Image") != "on" {
-			data2.Image = ""
-			data2.VisibleImage = "invisible"
-		}
-		if r.FormValue("Name") != "on" {
-			data2.Prénom = ""
-			data2.VisiblePrénom = "invisible"
-		}
-		if r.FormValue("CreationDate") != "on" {
-			data2.DateDeCréation = ""
-			data2.VisibleDateDeCréation = "invisible"
-		}
-		if r.FormValue("Members") != "on" {
-			data2.Membres = ""
-			data2.VisibleMembres = "invisible"
-		}
-		if r.FormValue("FirstAlbum") != "on" {
-			data2.PremierAlbum = ""
-			data2.VisiblePremierAlbum = "invisible"
-		}
-
-		PlacerUnePage(w, r, data2, "HTML/templateBlocSimple.html")
-	}
+	TexteListeMembre      string
 }
 
 func PlacerUnePage(w http.ResponseWriter, r *http.Request, data PageData, lienPage string) {
@@ -140,18 +87,4 @@ func PlacerUnePage(w http.ResponseWriter, r *http.Request, data PageData, lienPa
 	if err != nil {
 		log.Println("Erreur d'exécution du template:", err)
 	}
-}
-
-func ComplétéLaPageInformation(id int, listeID []int, lotDeListe LotDeListe, nomPage string, w http.ResponseWriter, r *http.Request) {
-	blocArtiste := TrouverUnElementParID_ArtisteS(listeID[id], lotDeListe.listeDesArtistes)
-
-	data2 := PageData{
-		Prénom:         blocArtiste.Name,
-		Image:          blocArtiste.Image,
-		DateDeCréation: strconv.Itoa(blocArtiste.CreationDate),
-		Membres:        blocArtiste.Members[0],
-		PremierAlbum:   blocArtiste.FirstAlbum,
-		Id:             strconv.Itoa(blocArtiste.Id),
-	}
-	PlacerUnePage(w, r, data2, nomPage)
 }
