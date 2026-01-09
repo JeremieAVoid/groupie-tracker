cb1c4da4b89b943bb6f699a4a10e5e4238bf31e9
pemerick <emerick.pacaud@ynov.com>
2026-01-09 08:04:26 +0100

    [Correction] Les liens entre les pages HTML et CSS sont recréé

--- main.go
+++ main.go
@@ -1,3 +1,96 @@
+package main
+
+import (
+	"fmt"
+	groupie "groupie/src"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("début")
+	// 1 Les fonctions :
+	lotDeListe := groupie.ChargerLesDonnées()
+	// lotDeListe := groupie.LotDeListe{}
+	listeID := []int{}
+
+	http.HandleFunc("/Rechercher", func(w http.ResponseWriter, r *http.Request) {
+		nombreAAfficherT := r.FormValue("nombreAAfficher")
+		nombreAAfficher := groupie.TransformerEnNombre(nombreAAfficherT)
+		listeID = groupie.Recherche(lotDeListe, r.FormValue("catégorie"), r.FormValue("recherche"), nombreAAfficher)
+		texte := ""
+		for i := 0; i < len(listeID); i++ {
+			texte += strconv.Itoa(listeID[i]) + "\n"
+		}
+		groupie.PlacerLesRésultaDeRecherche(w, r, listeID, lotDeListe)
+		// fmt.Fprintln(w, texte)
+	})
+
+	http.HandleFunc("/informationsAppelle", func(w http.ResponseWriter, r *http.Request) {
+		idT := r.FormValue("Id")
+		id, err := strconv.Atoi(idT)
+		if err != nil {
+			fmt.Println(idT)
+			fmt.Println("Problème !")
+			// panic(err)
+		} else {
+			groupie.ComplétéLaPageInformation(id-1, listeID, lotDeListe, "static/templates/Informations.html", w, r)
+		}
+		// w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// http.ServeFile(w, r, "HTML/Informations.html")
+	})
+
+	// 2 - Les CSS :
+	// http.HandleFunc("static/style/style.css", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	// 	http.ServeFile(w, r, "static/style/style.css")
+	// })
+	// http.HandleFunc("static/style/styleBarreDeRecherche.css", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	// 	http.ServeFile(w, r, "static/style/styleBarreDeRecherche.css")
+	// })
+	// http.HandleFunc("static/style/styleInformation.css", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	// 	http.ServeFile(w, r, "static/style/styleInformation.css")
+	// })
+	// http.HandleFunc("static/style/styleTemplate.css", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	// 	http.ServeFile(w, r, "static/style/styleTemplate.css")
+	// })
+
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+
+	http.HandleFunc("static/templates/Informations.html", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeFile(w, r, "static/templates/Informations.html")
+	})
+
+	// 3 - Démarer le serveur :
+
+	log.Println("Serveur lancé sur http://localhost:8080")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		http.ServeFile(w, r, "static/templates/main.html")
+	})
+
+	http.HandleFunc("/open", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		go func() {
+			_ = exec.Command("xdg-open", "http://localhost:8080/").Start()
+		}()
+		w.Write([]byte("Attempted to open browser"))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
 // package main
 
 // import (
@@ -75,90 +168,3 @@
 //         log.Println("Serveur lancé sur http://localhost:8080")
 //     }
 // }
-
-package main
-
-import (
-	"fmt"
-	groupie "groupie/src"
-	"log"
-	"net/http"
-	"os/exec"
-	"strconv"
-)
-
-func main() {
-	fmt.Println("début")
-	// 1 Les fonctions :
-	lotDeListe := groupie.ChargerLesDonnées()
-	// lotDeListe := groupie.LotDeListe{}
-	listeID := []int{}
-
-	http.HandleFunc("/Rechercher", func(w http.ResponseWriter, r *http.Request) {
-		nombreAAfficherT := r.FormValue("nombreAAfficher")
-		nombreAAfficher := groupie.TransformerEnNombre(nombreAAfficherT)
-		listeID = groupie.Recherche(lotDeListe, r.FormValue("catégorie"), r.FormValue("recherche"), nombreAAfficher)
-		texte := ""
-		for i := 0; i < len(listeID); i++ {
-			texte += strconv.Itoa(listeID[i]) + "\n"
-		}
-		groupie.PlacerLesRésultaDeRecherche(w, r, listeID, lotDeListe)
-		// fmt.Fprintln(w, texte)
-	})
-
-	http.HandleFunc("/informationsAppelle", func(w http.ResponseWriter, r *http.Request) {
-		idT := r.FormValue("Id")
-		id, err := strconv.Atoi(idT)
-		if err != nil {
-			fmt.Println(idT)
-			fmt.Println("Problème !")
-			// panic(err)
-		} else {
-			groupie.ComplétéLaPageInformation(id-1, listeID, lotDeListe, "static/templates/Informations.html", w, r)
-		}
-		// w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// http.ServeFile(w, r, "HTML/Informations.html")
-	})
-
-	// 2 - Les CSS :
-	http.HandleFunc("static/style/style.css", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/css; charset=utf-8")
-		http.ServeFile(w, r, "static/style/style.css")
-	})
-	http.HandleFunc("static/style/styleBarreDeRecherche.css", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/css; charset=utf-8")
-		http.ServeFile(w, r, "static/style/styleBarreDeRecherche.css")
-	})
-	http.HandleFunc("static/style/styleInformation.css", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/css; charset=utf-8")
-		http.ServeFile(w, r, "static/style/styleInformation.css")
-	})
-	http.HandleFunc("static/style/styleTemplate.css", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/css; charset=utf-8")
-		http.ServeFile(w, r, "static/style/styleTemplate.css")
-	})
-	http.HandleFunc("static/templates/Informations.html", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		http.ServeFile(w, r, "static/templates/Informations.html")
-	})
-
-	// 3 - Démarer le serveur :
-
-	log.Println("Serveur lancé sur http://localhost:8080")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		http.ServeFile(w, r, "static/templates/main.html")
-	})
-
-	http.HandleFunc("/open", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		go func() {
-			_ = exec.Command("xdg-open", "http://localhost:8080/").Start()
-		}()
-		w.Write([]byte("Attempted to open browser"))
-	})
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-}
