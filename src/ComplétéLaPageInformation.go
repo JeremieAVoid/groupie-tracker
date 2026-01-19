bc813efc2940e1059452814405276acca2cc1db3
pemerick <emerick.pacaud@ynov.com>
2026-01-19 14:35:00 +0100

    [Correction] correction d'un problème qui fesait que la page d'information renvoyais régulièrement une erreur

--- src/ComplétéLaPageInformation.go
+++ src/ComplétéLaPageInformation.go
@@ -31,6 +31,20 @@ func CrééLeTexteListeConcert(date DatesS, localisation LocationsS) string {
 }
 
 func ComplétéLaPageInformation(id int, listeID []int, lotDeListe LotDeListe, nomPage string, w http.ResponseWriter, r *http.Request) {
+	if len(listeID) == 0 {
+		for i := 0; i < len(lotDeListe.listeDesArtistes); i++ {
+			listeID = append(listeID, i)
+		}
+	}
+	if id < 0 {
+		id = 0
+	}
+	if id >= len(listeID) {
+		id = len(listeID) - 1
+	}
+	if id >= len(lotDeListe.listeDesArtistes) {
+		return
+	}
 	blocArtiste := TrouverUnElementParID_ArtisteS(listeID[id], lotDeListe.listeDesArtistes)
 	blocLocation := TrouverUnElementParID_LocationsS(listeID[id], lotDeListe.listeDesLocations)
 	blocDate := TrouverUnElementParID_DatesS(listeID[id], lotDeListe.listeDesDates)
